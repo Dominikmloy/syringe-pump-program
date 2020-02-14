49c3ac332f67abaa4323dfb4c27f9a54492fbb72
Dominik <dominikmloy@gmail.com>
2020-02-14 16:54:03 +0100

    added comments to increase readability of the code.

--- syringes.py
+++ syringes.py
@@ -1,15 +1,20 @@
+import Module_pumps as p
 
 
 class Syringes(object):
+    """
+    This class holds the specifications of all syringes. New syringes can be added to the
+    dictionary on the fly with the import_syringes function.
+    """
     def __init__(self):
+        # this dictionary maps the name of the syringe to its diameter in mm.
         self.syringes = {"Hamilton_1001TLL_1ml": 4.61, "Hamilton_1750TLL-XL_0.5ml": 3.26,
                          "Hamilton_1710TLL-XL_0.1ml": 1.46, "NormJect_tuberkulin_1ml": 4.7}
 
     def import_syringes(self, name, diameter):
+        # this function adds a new syringe to the dictionary 'self.syringes'.
         try:
             assert 0.1 < float(diameter) < 30.0
             self.syringes[name] = float(diameter)
         except AssertionError:
-            print("Diameter out of range. Accepted values: 0.1 - 30.0 cm. Syringe not imported.")
-            # logger_pump.warning("Diameter out of range. Accepted values: 0.1 - 30.0 cm. Syringe not imported.")
-            # logger would be better, dont know yet how to integrate in this module.
+            p.logger_pump.warning("Diameter out of range. Accepted values: 0.1 - 30.0 cm. Syringe not imported.")
