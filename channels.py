5e14536c09d55acfe62883e88aa4fb50e856cc8e
Dominik <dominikmloy@gmail.com>
2020-02-14 16:47:07 +0100

    added comments to increase readability of the code.

--- channels.py
+++ channels.py
@@ -13,26 +13,33 @@ class Channel(object):
         """
         self.volume_total = 0
         self.channel_volume = 0
-        self.interface_volume = 0
+        self.interface_volume = 0  # e.g. the tubing's volume leading to the channels
 
-        def _volume_channel():  # , section):
+        def _volume_channel():
             # volume = self.channel_x[section] * self.channel_y[section] * self.channel_z
             if self.volume_total != 0:
                 return self.volume_total
             else:
-                self.interface_volume = (self.inlets_number + self.outlets_number) * math.pi * self.interface_diameter ** 2
+                # calculate total interface volume
+                self.interface_volume = (self.inlets_number + self.outlets_number) * \
+                                        math.pi * self.interface_diameter ** 2
+                # Calculate the volume of each segment of the channel.
+                # Sum them up to get the total channel volume
                 for key in self.channel_x:
                     volume = self.channel_x[key] * self.channel_y[key] * self.channel_z
                     self.channel_volume += volume
+                # add the channel volume to the interface volume for the total volume of the chip
                 volume_total = self.channel_volume + self.interface_volume
                 return volume_total
-
+        # variables
         self.inlets_number = 0
         self.outlets_number = 0
         self.meanders_number = 0
         self.inlet_diameter = 0
         self.outlet_diameter = 0
         self.interface_diameter = 0
+        # dictionaries mapping the length in x, y, z direction of a section [mm] to
+        # the respective name of the section. name1-name2 denotes the section between the two names
         self.tubing_x = {"inlet_1-1": 0, "inlet_1-2": 0, "inlet_1-3": 0,
                          "inlet_2-1": 0, "inlet_2-2": 0, "outlet": 0}
         self.channel_x = {"inlet_1-1-mixing_1": 0, "inlet_1-2-mixing_1": 0,
@@ -46,15 +53,22 @@ class Channel(object):
                           "inlet_2-1-mixing_2": 0, "inlet_2-2-mixing_2": 0,
                           "mixing_2-meander_2": 0, "meander_2-meander_2": 0, "meander_2-outlet": 0}
         self.channel_z = 0
+        # reads the channel's specifications from the spec file and stores them in the variables
+        # defined under __init__.
         self._set_from_spec_file(filename)
-        self.volume_total = _volume_channel()  # Annahme: aendert sich nicht und wird nur einmal berechnet sobald channels gelesen wurden
+        self.volume_total = _volume_channel()
 
+    # calculates the volume of one inlet or outlet, under the assumption that inlet and outlet
+    # have the same diameter
     def volume_tubing(self, tubing):
         return self.tubing_x[tubing] * (self.inlet_diameter / 2) ** 2 * math.pi
 
+    # calculate the volume of one section of the channel. Assumption: channel height does not change.
     def volume_channel_section(self, section):
         return self.channel_x[section] * self.channel_y[section] * self.channel_z
 
+    # calculate the volume of the channels from the inlets to the mixing zone 2.
+    # this function is only needed for the double meander channel.
     def volume_to_mixing_2(self):
         sections = ["inlet_1-1-mixing_1", "inlet_1-2-mixing_1",
                     "inlet_1-3-mixing_1", "mixing_1-meander_1",
@@ -98,7 +112,6 @@ class Channel(object):
                     for j in self.tubing_x.keys():
                         if j in line:
                             self.tubing_x[j] = digit[0]
-                            # break  falls es nur einen Treffer pro zeile geben kann, aber ist wohl nicht so?
                         else:
                             pass
                 elif "channel_x" in line:
@@ -106,7 +119,6 @@ class Channel(object):
                     for j in self.channel_x.keys():
                         if j in line:
                             self.channel_x[j] = digit[0]
-                            # break  falls es nur einen Treffer pro zeile gibt
                         else:
                             pass
                 elif "channel_y" in line:
@@ -117,4 +129,4 @@ class Channel(object):
                         else:
                             pass
                 else:
-                    pass  # warning ausgeben wenn du eigentlich erwartest dass jede zeile geparst wird, aber ist ja vermutlich nicht so
+                    print('No information was extracted from this line: {}'.format(line))
